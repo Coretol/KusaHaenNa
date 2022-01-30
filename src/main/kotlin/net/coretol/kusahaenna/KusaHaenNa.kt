834c2fb44067d47a653b5bad1cce93777231578a
Propromp <p@propromp.net>
2022-01-30 16:36:19 +0900

    コードのクリーンアップ

--- src/main/kotlin/net/coretol/kusahaenna/KusaHaenNa.kt
+++ src/main/kotlin/net/coretol/kusahaenna/KusaHaenNa.kt
@@ -1,15 +1,15 @@
 package net.coretol.kusahaenna
 
 import net.coretol.kusahaenna.command.CommandKusa
-import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
-class KusaHaenNa: JavaPlugin() {
+class KusaHaenNa : JavaPlugin() {
     companion object {
-        lateinit var instance:KusaHaenNa
+        lateinit var instance: KusaHaenNa
     }
+
     override fun onEnable() {
-        instance=this
+        instance = this
         getCommand("kusa")!!.setExecutor(CommandKusa())
     }
 }
--- src/main/kotlin/net/coretol/kusahaenna/util/Util.kt
+++ src/main/kotlin/net/coretol/kusahaenna/util/Util.kt
@@ -3,27 +3,20 @@ package net.coretol.kusahaenna.util
 import com.google.common.collect.Queues
 import net.coretol.kusahaenna.KusaHaenNa
 import org.bukkit.Bukkit
-import org.bukkit.block.Block
 import java.util.*
-import java.util.concurrent.CompletableFuture
 
-fun async(function:() -> Unit) {
-    Bukkit.getScheduler().runTaskAsynchronously(KusaHaenNa.instance,function)
+fun async(function: () -> Unit) {
+    Bukkit.getScheduler().runTaskAsynchronously(KusaHaenNa.instance, function)
 }
-fun <T> runLater(delay:Long,function: () -> T):CompletableFuture<T> {
-    val future = CompletableFuture<T>()
-    Bukkit.getScheduler().runTaskLater(KusaHaenNa.instance, Runnable {
-        future.complete(function.invoke())
-    },delay)
-    return future
+
+fun sync(function: () -> Unit) {
+    Bukkit.getScheduler().runTask(KusaHaenNa.instance, function)
 }
-fun sync(function:() -> Unit) {
-    Bukkit.getScheduler().runTask(KusaHaenNa.instance,function)
-}
-fun <T> Queue<T>.splitQueue(count:Int = 1000):Queue<Queue<T>> {
+
+fun <T> Queue<T>.splitQueue(count: Int = 1000): Queue<Queue<T>> {
     val queues = Queues.newConcurrentLinkedQueue<Queue<T>>()
     run loop@{
-        while (true)  {
+        while (true) {
             val thousandQueue = Queues.newConcurrentLinkedQueue<T>()
             for (i in 0..count) {
                 val t = this.poll() ?: run {
